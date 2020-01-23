3655201369f14f838115d184049a8fed45d52574
ayphilly <aphilemon.aa@gmail.com>
2020-01-23 02:05:15 +0100

    first commit

---
+++ src/component/Error.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import error from '../svg/error.svg';
+import './error.css';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+export class Error extends Component {
+    render () {
+        return (
+            <div className="error">
+                <div className="error-box">
+                    <div className="error-svg">
+                        <img src={error} className="error-svg-im" alt="Brand"/>
+                    </div>
+                    <div className="error-text">
+                        <h3  >PAGE NOT FOUND!</h3>
+                        <Link className="error-link" to="/" style={{textDecoration:'none'}}>
+                            <FontAwesomeIcon className="lefty" icon={['fas', 'angle-double-left' ]} />
+                            HOME
+                        </Link>
+
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
